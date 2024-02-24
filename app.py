c5985b4b17ad8a2712301f90f677863ee3c4f142
SkylabMak <skylab135@gmail.com>
2024-02-24 10:12:41 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, request
+from flask_restful import reqparse, Api
+from werkzeug.exceptions import BadRequest
+from flask import jsonify
+import flask
+
+import numpy as np
+import pandas as pd
+import ast
+
+import os
+import json
+
+from model import predictEmp
+
+curr_path = os.path.dirname(os.path.realpath(__file__))
+
+feature_cols = ['department', 'education', 'recruitment_channel', 
+                'no_of_trainings', 'previous_year_rating', 
+                'length_of_service', 'awards_won', 
+                'avg_training_score']
+
+context_dict = {
+    'feats': feature_cols,
+    'zip': zip,
+    'range': range,
+    'len': len,
+    'list': list,
+}
+
+app = Flask(__name__)
+api = Api(app)
+
+# # FOR FORM PARSING
+# parser = reqparse.RequestParser()
+# parser.add_argument('list', type=list)
+
+# Define the parser and add the 'myList' argument
+parser = reqparse.RequestParser()
+parser.add_argument('list', type=list, location='json', required=True, help="List cannot be blank!")
+
+@app.errorhandler(BadRequest)
+def handle_bad_request(error):
+    response = jsonify({'message': error.data['message']})
+    response.status_code = 400
+    return response
+
+@app.route('/api/predict/json', methods=['POST'])
+def api_predict_json():
+    try:
+        # Parse input JSON
+        # data = flask.request.json
+        # Parse input JSON
+        data = request.json
+        input_list = data['list']
+        
+        # Convert input list to DataFrame without considering the index
+        input_df = pd.DataFrame([input_list], columns=feature_cols)
+        
+        print("data : ", input_df)
+        
+        # Predict
+        y_pred = predictEmp(input_df)
+        
+        # Convert prediction to int and respond
+        return jsonify({'message': "success", "pred":y_pred})
+    except Exception as e:
+        print("error in API ->", str(e))
+        return jsonify({'message': "error", "pred": "None", "e": str(e)}), 400
+
+@app.route('/api/predict/jsonList', methods=['POST'])
+def api_predict_json_list():
+    try:
+        # Extract the list of inputs from the request JSON
+        input_list = request.json['list']
+               
+        # Initialize an empty list to store predictions
+        predictions = []
+
+        # Loop through each input in the input list
+        for input_row in input_list:
+            # Convert the current input row to a pandas DataFrame
+            input_df = pd.DataFrame([input_row], columns=feature_cols)
+            
+            # Call predictEmp with the current input DataFrame
+            prediction = predictEmp(input_df)
+            
+            # Append the prediction to the predictions list
+            predictions.append(prediction.item())
+        
+        # Return the list of predictions as JSON
+        return jsonify({'message': "success", "pred": predictions})
+    except Exception as e:
+        print("error in api_predict_json_list ->", str(e))
+        return jsonify({'message': "error", 'error': str(e)}), 400
+    
+@app.route('/')
+def index():
+    return {'message':"use /api to use"}
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
---
+++ model.py
@@ -0,0 +1,59 @@
+import joblib 
+import pandas as pd
+import numpy as np
+import os
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+# load the model file
+curr_path = os.path.dirname(os.path.realpath(__file__))
+xgb_modelEmp = joblib.load(curr_path + "/model/xgb_model.joblib")
+fitted_pipeline = joblib.load(curr_path + "/model/fitted_pipeline.joblib")
+clustering  = joblib.load(curr_path + "/model/clustering_model.joblib")
+
+
+cat_columns = ["department","education","recruitment_channel","no_of_trainings","awards_won", "n_cluster"]
+num_columns = ["previous_year_rating","length_of_service","avg_training_score"]
+id_dep_columns = ["employee_id","is_promoted","gender","age","region"]
+feature_cols = ['department', 'education', 'recruitment_channel', 
+                'no_of_trainings', 'previous_year_rating', 
+                'length_of_service', 'awards_won', 
+                'avg_training_score','n_cluster']
+
+
+
+def predictEmp(attributes: pd.DataFrame):
+    try:
+        print("att : ", attributes)
+        
+        # Ensure the 'attributes' is a DataFrame
+        if not isinstance(attributes, pd.DataFrame):
+            attributes = pd.DataFrame(attributes, columns=feature_cols) # Adjust 'feature_cols' as necessary
+        
+        # Select columns for clustering
+        X_clus = attributes[["length_of_service", "avg_training_score", "awards_won"]]
+        
+        # Apply clustering model to generate cluster labels
+        n_cluster = clustering.predict(X_clus)
+        
+        # Add cluster labels to the attributes DataFrame
+        attributes['n_cluster'] = n_cluster
+        
+        # Make sure to reorder / select columns as expected by the pipeline if necessary
+        # Drop unnecessary columns if they are not expected by the fitted_pipeline
+        attributes_for_pipeline = attributes.drop(columns=id_dep_columns, errors='ignore')
+        
+        # Apply the fitted pipeline transformations
+        new_data_processed = fitted_pipeline.transform(attributes_for_pipeline)
+        
+        # Use the model to make predictions
+        predictions = xgb_modelEmp.predict(new_data_processed)
+        
+        print("predictions are ", predictions)
+        # Return the predictions
+        return predictions
+    except Exception as e:
+        print("error in model ->", e)
+        return None
